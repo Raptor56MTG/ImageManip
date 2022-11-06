daec17632a7a19c046a17ff5f9482505b8041b6a
Raptor56MTG <raptor56mtg@gmail.com>
2022-11-06 14:47:02 -0600

    updated code to be more elegant to read, and added blend + twist code

--- cogs/bot_commands.py
+++ cogs/bot_commands.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from .twistImage import twist
+from .twistImage import twist, blend
 from .config import image_types
 
 
@@ -11,33 +11,48 @@ class BotCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['T', 't', 'Twist', 'twist'])
-    async def twistImage(self, ctx, buffer=None):
-        """This takes in any image and outputs a twisted
-        version of that image."""
+    async def image_validation(self, ctx) -> bool:
+        """Determines if input is valid."""
         try:
             image_url = ctx.message.attachments[0].url
             if image_url.endswith(image_types):
                 await ctx.message.attachments[0].save('image.png')
-                if buffer is not None:
-                    if buffer.isnumeric():
-                        twist(int(buffer))
-                        await ctx.send(file=discord.File('image-twist.png'))
-                    else:
-                        await ctx.send("need an integer buffer value.")
-                else:
-                    twist()
-                    await ctx.send(file=discord.File('image-twist.png'))
+                return True
             else:
                 await ctx.send(discord.Embed(
                     title="Error",
                     description="The attachment provided was not an image.",
                     colour=discord.Color.blue()))
+                return False
         except IndexError:
             await ctx.send(embed=discord.Embed(
                 title="Error",
                 description="No image attached.",
                 colour=discord.Color.blue()))
+            return False
+
+    @commands.command(aliases=['T', 't', 'Twist', 'twist'])
+    async def twistImage(self, ctx, buffer=None):
+        """This takes in any image and outputs a twisted
+        version of that image."""
+        valid = await self.image_validation(ctx)
+        if valid:
+            if buffer is None:
+                twist()
+                await ctx.send(file=discord.File('final.png'))
+            elif buffer.isnumeric():
+                twist(int(buffer))
+                await ctx.send(file=discord.File('final.png'))
+            else:
+                await ctx.send("need an integer buffer value.")
+
+    @commands.command(aliases=['B', 'b', 'Blend', 'blend'])
+    async def blendImage(self, ctx, buffer=None):
+        """This takes in any image and outputs 180 degree blend."""
+        valid = await self.image_validation(ctx)
+        if valid:
+            blend()
+            await ctx.send(file=discord.File('final.png'))
 
 
 async def setup(bot):
--- cogs/twistImage.py
+++ cogs/twistImage.py
@@ -3,14 +3,20 @@ import math
 
 
 def twist(buffer=1):
-    """This takes an image and rotates it back and forth
+    """This takes an image and rotates 180 degrees
     moving inside towards the center at a pixel rate of the
     buffer value."""
-
     image1 = Image.open('image.png')
     width, height = image1.size
     min_center_dist = min(math.ceil(height / 2), math.ceil(width / 2))
     for i in range(0, min_center_dist, buffer):
         image2 = image1.crop((i, i, width - i, height - i)).transpose(3)
         image1.paste(image2, [i, i])
-    image1.save('image-twist.png')
+    image1.save('final.png')
+
+
+def blend():
+    """This takes an image and rotates 180
+    degrees and blends it wth the original."""
+    Image.blend(Image.open('image.png'),
+                Image.open('image.png').transpose(3), 0.5).save('final.png')
