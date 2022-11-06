daa30ad8cbce39384133ac14aca6169c2b5cf781
Raptor56MTG <raptor56mtg@gmail.com>
2022-11-06 15:04:48 -0600

    reordered helper functions into utils.py file

--- cogs/bot_commands.py
+++ cogs/bot_commands.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
 from .twistImage import twist, blend
-from .config import image_types
-from .utils import embed
+from .utils import embed, image_validation
 
 
 class BotCommands(commands.Cog):
@@ -12,25 +11,11 @@ class BotCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def image_validation(self, ctx) -> bool:
-        """Determines if input is valid."""
-        try:
-            image_url = ctx.message.attachments[0].url
-            if image_url.endswith(image_types):
-                await ctx.message.attachments[0].save("image.png")
-                return True
-            else:
-                await ctx.send(embed=embed("Error", "The attachment provided was not an image."))
-                return False
-        except IndexError:
-            await ctx.send(embed=embed("Error", "No image attached."))
-            return False
-
     @commands.command(aliases=["T", "t", "Twist", "twist"])
     async def twistImage(self, ctx, buffer=None):
         """This takes in any image and outputs a twisted
         version of that image."""
-        valid = await self.image_validation(ctx)
+        valid = await image_validation(ctx)
         if valid:
             if buffer is None:
                 twist()
@@ -44,7 +29,7 @@ class BotCommands(commands.Cog):
     @commands.command(aliases=["B", "b", "Blend", "blend"])
     async def blendImage(self, ctx, buffer=None):
         """This takes in any image and outputs 180 degree blend."""
-        valid = await self.image_validation(ctx)
+        valid = await image_validation(ctx)
         if valid:
             blend()
             await ctx.send(file=discord.File("final.png"))
--- cogs/utils.py
+++ cogs/utils.py
@@ -1,8 +1,24 @@
 import discord
+from .config import image_types
 
 
-def embed(title, description, color=discord.Color.blue()):
+def embed(title, description, color=discord.Color.blue()) -> object:
     """returns a discord embed object."""
     return discord.Embed(title=title,
                          description=description,
                          colour=color)
+
+
+async def image_validation(ctx) -> bool:
+    """Determines if input is valid."""
+    try:
+        image_url = ctx.message.attachments[0].url
+        if image_url.endswith(image_types):
+            await ctx.message.attachments[0].save("image.png")
+            return True
+        else:
+            await ctx.send(embed=embed("Error", "The attachment provided was not an image."))
+            return False
+    except IndexError:
+        await ctx.send(embed=embed("Error", "No image attached."))
+        return False
